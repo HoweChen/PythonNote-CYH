55910fc1939eaa60e857074d60a5a8021a0800d2
SkyFather <cyh278889789@gmail.com>
2016-11-17 20:05:41 +0800

    update

---
+++ Python Basic/code.py
@@ -0,0 +1,13 @@
+# print Ada Lovelace
+name = "ada lovelace"
+print(name.title())
+
+
+# print 'Pythonhaha'
+favourite_language = "Pythonhaha  "
+print("'" + favourite_language.rstrip() + "'")
+
+# print 'Pythonhaha'
+favourite_language = "  Pythonhaha"
+print("'" + favourite_language.lstrip() + "'")
+
