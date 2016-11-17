6ced462b6638964ee30ac3b9b4406864b76fe7f9
SkyFather <cyh278889789@gmail.com>
2016-11-17 20:44:03 +0800

    update

--- Python Basic/code.py
+++ Python Basic/code.py
@@ -11,3 +11,17 @@ print("'" + favourite_language.rstrip() + "'")
 favourite_language = "  Pythonhaha"
 print("'" + favourite_language.lstrip() + "'")
 
+# list
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)  # ['honda', 'yamaha', 'suzuki']
+motorcycles.append('fuck')
+print(motorcycles)  # ['honda', 'yamaha', 'suzuki', 'fuck']
+motorcycles.insert(0, 'test')
+print(motorcycles)  # ['test', 'honda', 'yamaha', 'suzuki', 'fuck']
+del motorcycles[0]
+print(motorcycles)  # ['honda', 'yamaha', 'suzuki', 'fuck']
+del motorcycles[motorcycles.__len__() - 1]
+print(motorcycles)  # ['honda', 'yamaha', 'suzuki']
+popped_element = motorcycles.pop()
+print(popped_element)  # suzuki
+print(motorcycles)  # ['honda', 'yamaha']
