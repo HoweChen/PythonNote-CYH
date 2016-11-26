f24ebcb3f7f1fb4ca466672d78f0f911da514daf
SkyFather <cyh278889789@gmail.com>
2016-11-26 18:12:25 +0800

    update

--- Python Basic/code.py
+++ Python Basic/code.py
@@ -25,3 +25,48 @@ print(motorcycles)  # ['honda', 'yamaha', 'suzuki']
 popped_element = motorcycles.pop()
 print(popped_element)  # suzuki
 print(motorcycles)  # ['honda', 'yamaha']
+cars = ['audi', 'subaru', 'bmw', 'toyata']
+cars.sort()
+print(cars)  # ['audi', 'bmw', 'subaru', 'toyata']
+cars.sort(reverse=True)
+print(cars)  # ['toyata', 'subaru', 'bmw', 'audi']
+print('Here is the original order')
+print(cars)  # ['toyata', 'subaru', 'bmw', 'audi']
+print('Here is the sorted order')
+print(sorted(cars))  # ['audi', 'bmw', 'subaru', 'toyata']
+print('Here is the original order again')
+print(cars)  # ['toyata', 'subaru', 'bmw', 'audi']
+print(sorted(cars, reverse=True))  # can also reverse in sorted()
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)  # ['bmw', 'audi', 'toyota', 'subaru']
+cars.reverse()
+print(cars)  # ['subaru', 'toyota', 'audi', 'bmw'] different from reverse=True
+print(len(cars))
+
+# Tranverese the list
+magicians = ['alice', 'david', 'carolina']
+for magican in magicians:
+    print(magican)
+    print(magican.title() + 'has done a great job')
+
+# range()
+for value in range(1, 5):
+    print(value)
+numbers = list(range(1, 6))
+print(numbers)
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+square = list()
+for value in range(1, 11):
+    value *= value  # or value = value**2
+    square.append(value)
+print(square)
+for value in square:
+    print(value)
+print('Min of square')
+print(min(square))
+print('Max of square')
+print(max(square))
+print('Sum of square')
+print(sum(square))
