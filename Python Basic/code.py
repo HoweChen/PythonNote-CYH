86144de1a18ec764758dfffb8b04f9978c618af3
SkyFather <cyh278889789@gmail.com>
2016-11-27 21:03:42 +0800

    update

--- Python Basic/code.py
+++ Python Basic/code.py
@@ -210,6 +210,4 @@ def describe_pet(animal_type, pet_name):
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
-describe_pet(animal_type='hamster', pet_name='harry') #keyword parameter
-
-
+describe_pet(animal_type='hamster', pet_name='harry')  # keyword parameter
