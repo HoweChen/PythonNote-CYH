d0f0585af60af7fdc9db5c13c3c3524c370222eb
SkyFather <cyh278889789@gmail.com>
2016-11-28 12:53:12 +0800

    update

--- Python Basic/code.py
+++ Python Basic/code.py
@@ -212,3 +212,19 @@ def describe_pet(animal_type, pet_name):
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
 describe_pet(animal_type='hamster', pet_name='harry')  # keyword parameter
+
+
+def print_models(unprinted_designs, completed_models):
+    """ 模拟打印每个设计ǈ直到ு有未打印的设计为止 打印每个设计后ǈ都将其移到列表completed_models中 """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        # 模拟根据设计制作3D打印模型的过程
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models):
+    """显示打印好的所有模型"""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
