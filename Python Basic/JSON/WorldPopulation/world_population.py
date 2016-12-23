947aab4ac1e38fd636aca2edbd734b49c011211f
SkyFather <cyh278889789@gmail.com>
2016-12-23 21:07:17 +0800

    update

--- Python Basic/JSON/WorldPopulation/world_population.py
+++ Python Basic/JSON/WorldPopulation/world_population.py
@@ -10,7 +10,9 @@ with open(filename) as fopen:
     # pop_data is a list
     # print(pop_data)
 
-pygal_data = {}
+data = {}
+
+
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
@@ -20,13 +22,22 @@ for pop_dict in pop_data:
         # print(code)
         if code:
             print(code + ': ' + str(population))
-            pygal_data[code] = population
+            data[code] = population
         else:
             print('Error - ' + country_name)
 
+data_1, data_2, data_3 = {}, {}, {}
+# group the population into 3 sub-groups
+for code, population in data.items():
+    if population < 10000000:
+        pass
+    else:
+        pass
+
+
 wm = pygal.maps.world.World()
 wm.title = 'World Population in 2010, by Country'
-wm.add('2010', pygal_data)
-# print([item for item in pygal_data.items()])
+wm.add('2010', data)
+# print([item for item in data.items()])
 
 wm.render_to_file('./Python Basic/JSON/WorldPopulation/world_population.svg')
