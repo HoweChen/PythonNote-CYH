d520cb8a1411361b648496b2000545bf70113338
SkyFather <cyh278889789@gmail.com>
2017-02-14 16:54:09 +0800

    numpy sample

--- Numpy/1.12/main.py
+++ Numpy/1.12/main.py
@@ -1,1 +1,13 @@
 import numpy
+import sys
+
+
+def numpysum(n):
+    a = numpy.arange(n)**2
+    b = numpy.arange(n)**3
+    c = a + b
+    return c
+
+
+size = int(sys.argv[1])
+print(numpysum(size))
