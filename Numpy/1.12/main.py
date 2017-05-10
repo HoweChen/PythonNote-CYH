e7bc2b0735d07f053ae77d12d210fbb073f5fdc6
SkyFather <cyh278889789@gmail.com>
2017-05-10 17:15:51 +0800

    update

--- Numpy/1.12/main.py
+++ Numpy/1.12/main.py
@@ -6,8 +6,11 @@ def numpysum(n):
     a = numpy.arange(n)**2
     b = numpy.arange(n)**3
     c = a + b
+    print(c.dtype)
     return c
 
 
-size = input("Please input the size")
+# size = input("Please input the size")
+size = 10
 print(numpysum(int(size)))
+
