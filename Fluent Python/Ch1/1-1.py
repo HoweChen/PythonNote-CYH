e3c5fe3d79848128b84967302ace27fcbb1d3b53
SkyFather <cyh278889789@gmail.com>
2017-05-15 21:20:29 +0800

    update with sorting deck method

--- Fluent Python/Ch1/1-1.py
+++ Fluent Python/Ch1/1-1.py
@@ -36,6 +36,36 @@ randomCards = [choice(deck) for i in range(10)]
 for itr, val in randomCards:
     print(itr + ' of ' + val)
 
+# print top 3 cards of the deck
+print(deck[:3])
+# iterable deck
+for card in deck:
+    print(card)
+
+for card in reversed(deck):
+    print(card)
+
+# keyword "in" is also available
+print(Card('Q', 'hearts') in deck)  # True
+print(Card('Q', 'fuck') in deck)  # False
+
+# sort the deck according to its suit_value and its rank_value
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+# use sorted with 'key' keyword to sort the whole tuple
+# according to this key, in this case is the rank_value returned by
+# spades_high funcion
+sorted_deck = sorted(deck, key=spades_high)
+
+for card in sorted_deck:
+    print(card)
+
 # The reason why choice(deck) would automatically worked on deck._cards is that
 #  we re-write the __getitem__ method so when we use random.choice() it calls
 # the __getitem__ method so that it would return the self._cards member
