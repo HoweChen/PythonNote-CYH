e6b3a80a977a0b1ef82215d7b454ce4cec4cacd5
SkyFather <cyh278889789@gmail.com>
2017-05-16 20:51:28 +0800

    update class Vector

--- Fluent Python/Ch1/1-2.py
+++ Fluent Python/Ch1/1-2.py
@@ -1,1 +1,22 @@
 from math import hypot
+
+
+class Vector:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector({0},{1})'.format(self.x, self.y)
+        # or return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+
+    def __mul__(self, scaler):
+        return Vector(self.x * scaler, self.y * scaler)
