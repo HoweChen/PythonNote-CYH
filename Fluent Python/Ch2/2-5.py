f6e9f7d6f8a4cf9d188d640a8eecedf47aae2427
SkyFather <cyh278889789@gmail.com>
2017-05-17 18:55:57 +0800

    update comment

--- Fluent Python/Ch2/2-5.py
+++ Fluent Python/Ch2/2-5.py
@@ -4,9 +4,9 @@ symbols = '$¢£¥€¤'
 symbols_tuple = tuple(ord(symbol) for symbol in symbols)
 
 # (ord(symbol) for symbol in symbols) actually returns a generator
-# tuple() accept one argument which could be an iterable object, generator is an
-# interable object, so it could be used in tuple(). Also, when there is only one
-# argument needed in the function call, the outside () of 
+# tuple() accept one argument which could be an iterable object, generator is
+# an interable object, so it could be used in tuple(). Also, when there is
+# only one argument needed in the function call, the outside () of
 # (ord(symbol) for symbol in symbols) could be ignored
 
 print(symbols_tuple)
