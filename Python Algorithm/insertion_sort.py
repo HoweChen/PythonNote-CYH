f9403bb2bdbe8d5ef6769fcd49a6a2bd82448f8e
SkyFather <cyh278889789@gmail.com>
2017-10-28 00:00:32 +0800

    update binary_search_insertion_sort

--- Python Algorithm/insertion_sort.py
+++ Python Algorithm/insertion_sort.py
@@ -3,29 +3,30 @@ import random
 
 
 def binary_search(target_list, target_number, left, right):
-    mid = int((left + right) / 2)
-    if right - left != 1:
+    if right - left == 1:
+        if target_list[left] < target_number < target_list[right]:
+            return right  # target number is within the scale
+        else:
+            return left  # the target number is smaller then the
+    elif left == right:
+        return left
+    else:
+        mid = int((left + right) / 2)
         if target_list[mid] < target_number:
-            # use mid instead of mid+1 because the value might be good to insert into mid and mid+1
             return binary_search(target_list, target_number, mid, right)
         elif target_number < target_list[mid]:
-            # use mid instead of mid-1 because the value might be good to insert into mid and mid+1
             return binary_search(target_list, target_number, left, mid)
-        else:
-            return mid
-    else:
-        return mid  # which is left because if right - left == 1 then mid == left
 
 
 def binary_search_insertion_sort(input_list):
+    # if input_list[1] < input_list[0]:
+    #     input_list[1], input_list[0] = input_list[0], input_list[1]
     list_length = len(input_list)
     for i in range(1, list_length):
         # when current number is smaller, do the binary_search to find the best location to insert
         if input_list[i] < input_list[i - 1]:
             index = binary_search(input_list, input_list[i], 0, i - 1)
-            for j in range(i, index + 1,
-                           -1):  # we use index+1 here because everytime it change the value between j and j-1
-                # do the insert job by swapping the element inside the list
+            for j in range(i, index, -1):
                 input_list[j], input_list[j - 1] = input_list[j - 1], input_list[j]
         else:
             continue
@@ -40,25 +41,28 @@ def insertion_sort(input_list):
                 # do the insert job by swapping the element inside the list
                 input_list[j], input_list[j - 1] = input_list[j - 1], input_list[j]
             else:
+
                 break
     return input_list
 
 
 def main():
-    # random_number_list = create_random_number_list.main()
+    random_number_list = create_random_number_list.main()
 
     # setup a random list from 0 to 10
-    random_number_list = list(range(10))
-    random.shuffle(random_number_list)
+    # random_number_list = list(range(10))
+    # random.shuffle(random_number_list)
 
     random_number_list_copy = random_number_list.copy()
-    print(random_number_list)
+    # print(random_number_list)
 
     # insertion_sort
+    # random_number_list = [6, 2, 8, 4, 1, 3, 0, 5, 7, 9]
     print(insertion_sort(random_number_list))
 
     # binary_search_insertion_sort
-    random_number_list_copy = [0, 3, 5, 7, 8, 6, 1, 4, 2, 9]
+    # random_number_list_copy = [6, 2, 8, 4, 1, 3, 0, 5, 7, 9]
+    # print(random_number_list_copy)
     print(binary_search_insertion_sort(random_number_list_copy))
 
 
