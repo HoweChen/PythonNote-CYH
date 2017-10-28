cad0712c481cb12069929d48fbb5d113c5558ab5
SkyFather <cyh278889789@gmail.com>
2017-10-28 18:50:41 +0800

    create with cocktail_sort

---
+++ Python Algorithm/cocktail_sort.py
@@ -0,0 +1,34 @@
+import create_random_number_list
+import random
+
+random_number_list = create_random_number_list.main()
+
+
+# 在每一次迭代中，大的，次大的，次次大的元素都会被换到对应的位置，然后最小的元素总是会被放到最上层
+# 因为第一次循环之后第二次都是从头开始，所以最小的数在中间也会被换到第一个去
+
+def bubble_sort(test_list):
+    list_length = len(test_list)
+
+    for i in range(list_length):
+        for j in range(list_length - 1):
+            if test_list[i] < test_list[j]:
+                test_list[i], test_list[j] = test_list[j], test_list[i]
+
+        # 预先把后面的数组给排个序
+        for j in range(list_length - 2, i, -1):
+            if test_list[j] < test_list[j - 1]:
+                test_list[j], test_list[j - 1] = test_list[j - 1], test_list[j]
+
+    return test_list
+
+
+def main():
+    random_number_list = list(range(10))
+    random.shuffle(random_number_list)
+    result = bubble_sort(random_number_list)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
