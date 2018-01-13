c873abe8c780ad0d220608ba057d1cfa82e72add
SkyFather <cyh278889789@gmail.com>
2018-01-13 21:55:14 +0800

    update with higher iteration

--- machine_learning/linear_regression.py
+++ machine_learning/linear_regression.py
@@ -5,6 +5,9 @@ from matplotlib import pyplot as plt
 x_train = [1, 2, 3]
 y_train = [1, 2, 3]
 
+x_train_placeholder = tf.placeholder(tf.float32)
+y_train_placeholder = tf.placeholder(tf.float32)
+
 W = tf.Variable(tf.random_normal([1]), name='weight')
 b = tf.Variable(tf.random_normal([1], name='bias'))
 
@@ -18,7 +21,16 @@ train = optimizer.minimize(cost)
 with tf.Session()as sess:
     sess.run(tf.global_variables_initializer())
 
-    for step in range(2001):
+    for step in range(10001):
         sess.run(train)
-        if step % 20 == 0:
+        if step % 50 == 0:
             print(step, sess.run(cost), sess.run(W), sess.run(b))
+
+    print('\n--------------------------\n')
+
+    for steps in range(10001):
+        cost_val, W_val, b_val, _ = sess.run([cost, W, b, train],
+                                             feed_dict={x_train_placeholder: [1, 2, 3], y_train_placeholder: [1, 2, 3]})
+
+        if steps % 50 == 0:
+            print(steps, cost_val, W_val, b_val)
