9f910725fcb4b01cb839235f4d4e9c05db113d78
SkyFather <cyh278889789@gmail.com>
2018-01-13 21:42:42 +0800

    linear regression

--- machine_learning/linear_regression.py
+++ machine_learning/linear_regression.py
@@ -2,4 +2,23 @@ import tensorflow as tf
 import numpy
 from matplotlib import pyplot as plt
 
-print('Hello Python Science')
+x_train = [1, 2, 3]
+y_train = [1, 2, 3]
+
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1], name='bias'))
+
+hypothesis = x_train * W + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+with tf.Session()as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(2001):
+        sess.run(train)
+        if step % 20 == 0:
+            print(step, sess.run(cost), sess.run(W), sess.run(b))
