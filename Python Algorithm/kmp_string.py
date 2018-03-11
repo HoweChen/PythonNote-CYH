def2fbec7fea44862da0ba6b24e553002538bce3
SkyFather <cyh278889789@gmail.com>
2018-03-11 11:12:49 +0800

    update KMP with comment

--- Python Algorithm/kmp_string.py
+++ Python Algorithm/kmp_string.py
@@ -1,3 +1,6 @@
+"""Idea and test case comes from http://www.ruanyifeng.com/blog/2013/05/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm.html"""
+
+
 def partial_match_table(target, stop_flag):
     target = str(target)
     stop_flag = int(stop_flag)
@@ -11,21 +14,22 @@ def partial_match_table(target, stop_flag):
             else:
                 return 0
         else:
+            # setup the prefix and postfix
             prefix = target[0]
             postfix = target[stop_flag]
+            # initialize a two list with prefix and postfix as each element
             foward_list = [prefix]
             backward_list = [postfix]
+            # get the full-list
             for i in range(1, stop_flag):
                 foward_list.append(foward_list[-1] + target[i])
             for j in range(stop_flag - 1, 0, -1):
                 backward_list.append(target[j] + backward_list[-1])
+            # get the set of two lists to do the intersection job
             foward_set = set(foward_list)
             backward_set = set(backward_list)
             intersection = foward_set.intersection(backward_set)
-            # if len(intersection) == 0:
-            #     return 0
-            # else:
-            #     return len(max(intersection))
+
             return len(max(intersection)) if len(intersection) > 0 else 0
 
 
@@ -38,13 +42,17 @@ def kmp(*args):
     for i in range(suspect_length):
         if suspect[i] == target[found_index]:
             found_index += 1
+            # if the found_index == length of the targt means that the target is found
             if found_index == len(target):
                 return "Found, and the index is {0}".format(i - target_length + 1)
             else:
+                # else, since the target[found_index] == suspect[i], continue the loop
                 continue
         else:
+            # if found_index==0,means no matching, continue the loop, if the found_index!=0, means there is match before, and now found the character not match
             if found_index != 0:
                 stop_flag = found_index - 1
+                # get the table_result from the partial match table algorithm
                 table_result = partial_match_table(target, stop_flag)
                 # move the suspect forward, by using the table_result as its index
                 while (suspect[i] != target[table_result]):
@@ -52,9 +60,11 @@ def kmp(*args):
                         break
                     else:
                         table_result = partial_match_table(target, stop_flag=table_result)
+                # if the target move forward, and the index at the suspect match the new target[table_result], update the found_index to the correct value, which is the table_result + 1
                 if suspect[i] == target[table_result]:
                     found_index = table_result + 1
                 else:
+                    # update the found_index to 0
                     found_index = 0
     return False
 
