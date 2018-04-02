908ba80ef1e8e3a1e0c6da3c46479b1f7491cb19
SkyFather <cyh278889789@gmail.com>
2018-04-02 20:42:00 +0800

    deep learning with tensorflow

--- machine_learning/cnn_tf.py
+++ machine_learning/cnn_tf.py
@@ -4,3 +4,6 @@ from matplotlib import pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 
 data = input_data.read_data_sets("data/MNIST/", one_hot=True)
+
+with tf.Session() as sess:
+    print(type(data.test.images[:100]))
