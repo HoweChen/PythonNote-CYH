fe4187e597c7b02ae474c3ed21d9f7dda5aa65f3
SkyFather <cyh278889789@gmail.com>
2018-04-13 20:47:14 +0800

    update shell_sort

--- Python Algorithm/shell_sort.py
+++ Python Algorithm/shell_sort.py
@@ -10,23 +10,24 @@ def shell_sort(input_list):
         step = list_length // 2
         while step > 0:
             for index in range(step, list_length):
-                temp = index #先记录下来
+                temp = index  # 先记录下来
                 for jump2index in range(index - step, -1, -step):
                     if input_list[temp] < input_list[jump2index]:
                         # 之所以是用temp的来比，是因为temp所在的index会被更新，这样小的数字会被一直交换下去
                         input_list[temp], input_list[jump2index] = input_list[jump2index], input_list[temp]
-                        temp = jump2index # 更新temp到已经换到的jump2index位置，以供下次交换
+                        temp = jump2index  # 更新temp到已经换到的jump2index位置，以供下次交换
             step = step // 2
         return input_list
 
 
 def main():
-    # rand_num_list = create_random_number_list.main()
+    rand_num_list = create_random_number_list.main()
     # rand_num_list = list(range(1, 11))
-    # random.shuffle(rand_num_list)
-    rand_num_list = [9, 6, 4, 8, 2, 10, 5, 3, 1, 7]
+    random.shuffle(rand_num_list)
+    backup = rand_num_list.copy()
     result = shell_sort(rand_num_list)
     print(result)
+    print(result == sorted(backup))
 
 
 if __name__ == '__main__':
