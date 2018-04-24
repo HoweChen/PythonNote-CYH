f9f1fef70044e49034fee909cbf9a78eb407e517
SkyFather <cyh278889789@gmail.com>
2018-04-24 15:28:59 +0800

    update with python hash_table sample

---
+++ Data Structure/hash_table.py
@@ -0,0 +1,28 @@
+class HashTable(object):
+    def __init__(self, *args, **kwargs):
+        length = kwargs.get('length')
+        input_list = kwargs.get('input_list')
+        method = kwargs.get('method')
+        self.table = list(range(length))
+        if input_list:
+            if not method or method == 'linked':
+                # this is the default method which is to create a linked list
+                self._push_linked(input_list)
+        else:
+            pass
+
+    def _push_linked(self, input_list):
+        print(input_list)
+
+    def _extend(self, size=None):
+        if not size:
+            self.table.extend(list(range(size)))
+
+
+def main():
+    # h_t = HashTable(length=10, input_list=list(range(10)))
+    h_t = HashTable(length=10, input_list=None, method='linked')
+
+
+if __name__ == '__main__':
+    main()
