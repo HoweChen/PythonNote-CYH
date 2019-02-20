4049b0247b46643b9bdfba440b57d7922478282d
HoweChen <yuhao.howe.chen@gmail.com>
2019-02-20 17:22:19 +0800

    update with class instance and write the time wrapper

--- Multi-process/produce_consume_show.py
+++ Multi-process/produce_consume_show.py
@@ -1,57 +1,71 @@
 from multiprocessing import Pool, Queue, Process
 import time
-
-q = Queue()
-main_q = Queue()
+from functools import wraps
 
 
-def producer():
-    for i in range(1000):
-        q.put(i)
-    q.put("None")
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        t_start = time.time()
+        f(*args, **kwargs)
+        t_end = time.time()
+        print(
+            f"func: {f.__name__}\nargs:[{args}, {kwargs}]\ntook: {t_end-t_start} sec")
+    return wrap
 
 
-def consumer():
-    meet_end = False
-    while True:
-        if q.empty() is True and meet_end is False:
-            # which means still need to wait for the stuff produced
-            continue
-        if q.empty() is True and meet_end is True:
-            break
-        result = q.get()
-        if result == "None":
-            meet_end = True
-            main_q.put("None")
-        else:
-            # print(result)
-            main_q.put(result)
+class Main(object):
+    def __init__(self, *args, **kwargs):
+        self.q = Queue()
+        self.main_q = Queue()
+
+    @timing
+    def start(self):
+        p = Process(target=self.producer)
+        c = Process(target=self.consumer)
+        start = time.time()
+        processes = [p, c]
+        for process in processes:
+            process.start()
+
+        self.show()
+
+        for process in processes:
+            process.join()
+        end = time.time()
+        # print(end - start)
+
+    def producer(self):
+        for i in range(10000):
+            self.q.put(i)
+        self.q.put(None)
+
+    def consumer(self):
+        while True:
+            if self.q.empty() is True:
+                # which means still need to wait for the stuff produced
+                continue
+            else:
+                result = self.q.get()
+
+                if result is not None:
+                    # print(result)
+                    self.main_q.put(result)
+                else:
+                    self.main_q.put(None)
+                    break
+
+    def show(self):
+        while True:
+            if self.main_q.empty() is True:
+                continue
+            else:
+                result = self.main_q.get()
+                if result is not None:
+                    print(result)
+                else:
+                    break
 
 
-def show():
-    main_q_end = False
-    while True:
-        if main_q.empty() is True and main_q_end is False:
-            continue
-        if main_q.empty() is True and main_q_end is True:
-            break
-        result = main_q.get()
-        if result == "None":
-            main_q_end = True
-        else:
-            print(result)
-
-
-p = Process(target=producer)
-c = Process(target=consumer)
-start = time.time()
-p.start()
-c.start()
-
-show()
-
-
-p.join()
-c.join()
-end = time.time()
-print(end - start)
+main = Main()
+main.start()
